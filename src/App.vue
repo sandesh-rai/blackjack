8a544cc90d93e6648bb3582c79b2e2845cf8f3f5
Sandesh <sandesh.thulung.rai@gmail.com>
2022-12-28 05:27:48 +0000

    Change font style

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ html, body {
 
 body {
   background: radial-gradient(ellipse at bottom, darkgreen 0%, #030617 100%);
+  font-family: monospace, Helvetica,Arial, sans-serif;
 }
 
 button {
@@ -36,6 +37,7 @@ button {
     font-weight: 700;
     box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
     cursor: pointer;
+    font-family: monospace, Helvetica, Arial, sans-serif;
 }
 
 button:hover, button:focus-visible {
