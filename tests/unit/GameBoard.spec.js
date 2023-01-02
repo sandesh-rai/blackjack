a4390b06b6e56d35030892426f5b0e4314acbc5c
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-02 00:19:59 +0000

    Add test to check 2 cards each are dealt after starting game

--- tests/unit/GameBoard.spec.js
+++ tests/unit/GameBoard.spec.js
@@ -1,5 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import GameBoard from '@/components/GameBoard.vue'
+import Vue from 'vue'
 
 describe('GameBoard.vue', () => {
   it('checks 52 cards are generated', () => {
@@ -7,5 +8,33 @@ describe('GameBoard.vue', () => {
     const wrapper = shallowMount(GameBoard)
 
     expect(wrapper.vm.$data.deckCards.length).toBe(52)
+  })
+
+  it('deals 2 cards each to player and dealer after starting game', async () => {
+    const wrapper = mount(GameBoard)
+
+    jest.useFakeTimers();
+    
+    // No cards have yet been dealt to player or dealer
+    expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
+    expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
+
+    // Trigger start game button
+    expect(wrapper.findAll('button').at(1).text()).toBe('Start game')
+    await wrapper.findAll('button').at(1).trigger('click')
+
+    // Fake first animation delay timer that gets called when start button is pressed
+    jest.runOnlyPendingTimers()
+    await Vue.nextTick()
+
+    // Fake animation delay timers for first 4 cards dealt on start
+    for (let i = 0; i < 4; i++) {
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
+    }
+    
+    // Player and dealer should now have 2 cards each
+    expect(wrapper.vm.$data.dealer.hand.length).toBe(2)
+    expect(wrapper.vm.$data.dealer.hand.length).toBe(2)
   })
 })
