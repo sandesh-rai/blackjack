0664d355fe6c33b561368df2f26ff09a253984fc
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-02 01:02:52 +0000

    Add test for checking that hit button adds card to hand

--- tests/unit/GameBoard.spec.js
+++ tests/unit/GameBoard.spec.js
@@ -31,10 +31,47 @@ describe('GameBoard.vue', () => {
     for (let i = 0; i < 4; i++) {
         jest.runOnlyPendingTimers()
         await Vue.nextTick()
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
     }
     
     // Player and dealer should now have 2 cards each
     expect(wrapper.vm.$data.dealer.hand.length).toBe(2)
     expect(wrapper.vm.$data.dealer.hand.length).toBe(2)
+  })
+
+  it('checks clicking hit button adds a card to the hand', async () => {
+    const wrapper = mount(GameBoard)
+
+    jest.useFakeTimers();
+
+    // Trigger start game button
+    expect(wrapper.findAll('button').at(1).text()).toBe('Start game')
+    await wrapper.findAll('button').at(1).trigger('click')
+
+    // Fake first animation delay timer that gets called when start button is pressed
+    jest.runOnlyPendingTimers()
+    await Vue.nextTick()
+
+    // Fake animation delay timers for first 4 cards dealt on start
+    for (let i = 0; i < 4; i++) {
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
+    }
+
+    // Player should only have 2 cards
+    expect(wrapper.vm.$data.player.hand.length).toBe(2)
+
+    // Check HIT button is not disabled
+    expect(wrapper.vm.$data.disableControl).toBe(false)
+
+    // Click HIT button
+    expect(wrapper.findAll('button').at(1).text()).toBe('HIT')
+    await wrapper.findAll('button').at(1).trigger('click')
+
+    // Player should now have 3 cards
+    expect(wrapper.vm.$data.player.hand.length).toBe(3)
   })
 })
