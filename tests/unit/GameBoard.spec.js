4f9e3ece1523f7abe682e204797743b60c331ec1
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-02 19:50:23 +0000

    Test that the correct player and dealer scores are calculated and shown

--- tests/unit/GameBoard.spec.js
+++ tests/unit/GameBoard.spec.js
@@ -3,37 +3,50 @@ import GameBoard from '@/components/GameBoard.vue'
 import Vue from 'vue'
 
 describe('GameBoard.vue', () => {
-  it('checks 52 cards are generated', () => {
+    let wrapper
 
-    const wrapper = shallowMount(GameBoard)
+    async function startGame (){
+        jest.useFakeTimers();
+    
+        // No cards have yet been dealt to player or dealer
+        expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
+        expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
+
+        // Trigger start game button
+        expect(wrapper.findAll('button').at(1).text()).toBe('Start game')
+        await wrapper.findAll('button').at(1).trigger('click')
+
+        // Fake first animation delay timer that gets called when start button is pressed
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
+
+        // Fake animation delay timers for first 4 cards dealt on start
+        for (let i = 0; i < 4; i++) {
+            jest.runOnlyPendingTimers()
+            await Vue.nextTick()
+            jest.runOnlyPendingTimers()
+            await Vue.nextTick()
+        }
+
+        jest.runOnlyPendingTimers()
+        await Vue.nextTick()
+    } 
+
+    beforeEach(() => {
+        wrapper = mount(GameBoard)
+    })
+    afterEach(() => {
+        wrapper.destroy();
+        wrapper = null;
+    });
+      
+  it('checks 52 cards are generated', () => {
 
     expect(wrapper.vm.$data.deckCards.length).toBe(52)
   })
 
   it('deals 2 cards each to player and dealer after starting game', async () => {
-    const wrapper = mount(GameBoard)
-
-    jest.useFakeTimers();
-    
-    // No cards have yet been dealt to player or dealer
-    expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
-    expect(wrapper.vm.$data.dealer.hand.length).toBe(0)
-
-    // Trigger start game button
-    expect(wrapper.findAll('button').at(1).text()).toBe('Start game')
-    await wrapper.findAll('button').at(1).trigger('click')
-
-    // Fake first animation delay timer that gets called when start button is pressed
-    jest.runOnlyPendingTimers()
-    await Vue.nextTick()
-
-    // Fake animation delay timers for first 4 cards dealt on start
-    for (let i = 0; i < 4; i++) {
-        jest.runOnlyPendingTimers()
-        await Vue.nextTick()
-        jest.runOnlyPendingTimers()
-        await Vue.nextTick()
-    }
+    await startGame()
     
     // Player and dealer should now have 2 cards each
     expect(wrapper.vm.$data.dealer.hand.length).toBe(2)
@@ -41,25 +54,7 @@ describe('GameBoard.vue', () => {
   })
 
   it('checks clicking hit button adds a card to the hand', async () => {
-    const wrapper = mount(GameBoard)
-
-    jest.useFakeTimers();
-
-    // Trigger start game button
-    expect(wrapper.findAll('button').at(1).text()).toBe('Start game')
-    await wrapper.findAll('button').at(1).trigger('click')
-
-    // Fake first animation delay timer that gets called when start button is pressed
-    jest.runOnlyPendingTimers()
-    await Vue.nextTick()
-
-    // Fake animation delay timers for first 4 cards dealt on start
-    for (let i = 0; i < 4; i++) {
-        jest.runOnlyPendingTimers()
-        await Vue.nextTick()
-        jest.runOnlyPendingTimers()
-        await Vue.nextTick()
-    }
+    await startGame()
 
     // Player should only have 2 cards
     expect(wrapper.vm.$data.player.hand.length).toBe(2)
@@ -73,5 +68,68 @@ describe('GameBoard.vue', () => {
 
     // Player should now have 3 cards
     expect(wrapper.vm.$data.player.hand.length).toBe(3)
+  })
+
+  it('checks correct player score is calculated after starting game', async () => {
+    await startGame()
+
+    let expectedScorePlayer = 0
+
+    // Calculate player expected score
+    for (let i = 0; i < wrapper.vm.$data.player.hand.length; i++) {
+        let cardVal = wrapper.vm.$data.player.hand[i].slice(1)
+
+        if (["K", "Q", "J"].includes(cardVal)) {
+            expectedScorePlayer += 10
+        } else if (cardVal === "A"){
+            // Ace will be scored as 11 initially as adding it to hand with start 2 cards will never make it exceed 21
+            expectedScorePlayer += 11
+        } else {
+            // Number card
+            expectedScorePlayer += parseInt(cardVal)
+        }
+    }
+
+    // Player score
+    expect(wrapper.vm.$data.player.score).toBe(expectedScorePlayer)
+    expect(wrapper.findAll('h2').at(1).html().replace(/\s/g, '')).toContain(`<h2>Player'sHand:${expectedScorePlayer}</h2>`) 
+  })
+
+  it('checks correct dealer score is calculated after starting game', async () => {
+
+    await startGame()
+
+    let expectedScoreDealer = 0
+    let displayedScoreDealer = 0
+
+    let cardVal = wrapper.vm.$data.dealer.hand[0].slice(1)
+
+    // Calculate player expected score
+    for (let i = 0; i < wrapper.vm.$data.dealer.hand.length; i++) {
+        let cardVal = wrapper.vm.$data.dealer.hand[i].slice(1)
+
+        if (["K", "Q", "J"].includes(cardVal)) {
+            expectedScoreDealer += 10
+        } else if (cardVal === "A"){
+            // Ace will be scored as 11 initially as adding it to hand with start 2 cards will never make it exceed 21
+            expectedScoreDealer += 11
+        } else {
+            // Number card
+            expectedScoreDealer += parseInt(cardVal)
+        }
+
+        // Displayed score on game start is only the 1st card
+        if (i === 0) {
+            displayedScoreDealer = expectedScoreDealer
+        } 
+    }
+
+    // Dealer actual score
+    expect(wrapper.vm.$data.dealer.score).toBe(expectedScoreDealer)
+    
+    // Dealer displayed score (first card score only)
+    expect(wrapper.vm.$data.dealer.firstCardScore).toBe(displayedScoreDealer) 
+    expect(wrapper.findAll('h2').at(0).html().replace(/\s/g, '')).toContain(`<h2>Dealer'sHand:${displayedScoreDealer}</h2>`)
+        
   })
 })
